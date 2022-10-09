f19319162236f99eeecbdf79664d77f74a21c969
Tri Pham <trimpham98@gmail.com>
2022-10-09 08:06:32 -0700

    Add print, if statement, for and while loop demonstration

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// Print to the console
 console.log("Hello World");
+
+// JS if statement
+if (true) {
+  console.log("Hello World");
+}
+
+// JS for loop
+for (var i = 0; i < 10; i++) {
+  console.log("Hello World");
+}
+
+// JS while loop
+var i = 0;
+while (i < 10) {
+  console.log("Hello World");
+  i++;
+}
+
+// JS function
+
+// JS object
+
+// JS array
+
+// JS class
+
+// JS class inheritance
+
+// JS class method
+
+// JS class property
