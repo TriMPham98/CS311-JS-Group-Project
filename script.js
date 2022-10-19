cafe8de8f3134ed0fa31cc5d8d974dfd9a111bfd
Tri Pham <trimpham98@gmail.com>
2022-10-18 22:22:29 -0700

    Remove TODO comments

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 // TODO: JavaScript print to console
 console.log("Hello world")
-
-
-// TODO: JavaScript expression
-
-
-
-// TODO: JavaScript if, else if, else conditionals
-
-
-
-// TODO: JavaScript loops
-
-
-
-// TODO: JavaScript subprograms
